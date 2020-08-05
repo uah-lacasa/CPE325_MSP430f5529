37a54a62187cbd6dc3d64e6574214ede003baa60
The LaCASA Laboratory <64563109+uah-lacasa@users.noreply.github.com>
2020-08-05 12:03:45 -0500

    Update Lab3_D3.c

--- Lab3/Lab3_D3.c
+++ Lab3/Lab3_D3.c
@@ -1,10 +1,11 @@
 /*------------------------------------------------------------------------------
  * File:        Lab3_D3.c (CPE 325 Lab3 Demo code)
- * Function:    Turning on LED1 when SW1 is pressed (MPS430F5529)
- * Description: This C program turns on LED1 connected to P1.0 when the SW1 is
- *              pressed. SW1 is connected to P2.1 and when the switch is pressed
- *              it is logic 0 (check the schematic). To avoid faulty detection
- *              of switch press delay of 20ms is added before turning on the LED1.
+ * Function:    Turning on LED1 when S1 is pressed (MSP-EXP430F5529LP)
+ * Description: This C program turns on LED1 connected to P1.0 when the S1 is
+ *              pressed. S1 is connected to P2.1 and when it is pressed, 
+ *              P2IN, bit 1 is read as a logic 0 (check the schematic). 
+ *              To avoid faulty detection of button press, 
+ *              debouncing delay of 20ms is added before turning on the LED1.
  * Clocks:      ACLK = 32.768kHz, MCLK = SMCLK = default DCO (~1 MHz)
  *
  *                           MSP430F552x
@@ -15,14 +16,14 @@
  *                      |             P1.0|-->LED1(RED)
  *                      |             P2.1|<-- SW1
  *                      |                 |
- * Input:       Press SW1
+ * Input:       Press S1
  * Output:      LED1 is turned on when SW1 is pressed
  * Authors:     Aleksandar Milenkovic, milenkovic@computer.org
  *              Prawar Poudel
  *------------------------------------------------------------------------------*/
 #include <msp430.h>
 
-#define SW1 P2IN&BIT1
+#define S1 P2IN&BIT1
 
 void main(void)
 {
@@ -36,13 +37,13 @@ void main(void)
 
     unsigned int i = 0;
     for (;;) {                            // Infinite loop
-        if ((SW1) == 0) {                 // If SW1 is pressed
+        if ((S1) == 0) {                  // If S1 is pressed
             for (i = 2000; i > 0; i--);   // Debounce ~20 ms
-            if ((SW1) == 0)
+            if ((S1) == 0)
             {
-                P1OUT |= BIT0;            // SW1 pressed, turn LED1 on
+                P1OUT |= BIT0;            // S1 pressed, turn LED1 on
             }
-            while((SW1)==0);              // Hang-on as long as SW1 pressed
+            while((S1)==0);              // Hang-on as long as SW1 pressed
         }else
             P1OUT &= ~BIT0;
     }
