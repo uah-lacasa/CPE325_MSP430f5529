342f6049bad04b9912c4657449ea7415be7785c7
The LaCASA Laboratory <64563109+uah-lacasa@users.noreply.github.com>
2020-08-05 11:12:36 -0500

    Update Lab3_D2.c

--- Lab3/Lab3_D2.c
+++ Lab3/Lab3_D2.c
@@ -1,6 +1,6 @@
 /*------------------------------------------------------------------------------
  * File:        Lab3_D2.c (CPE 325 Lab3 Demo code)
- * Function:    Blinking LED1 and LED2 (MPS430F5529)
+ * Function:    Blinking LED1 and LED2 (MSP-EXP430F5529LP)
  * Description: This C program toggle LED1 and LED2 at 1Hz by xoring P1.0 and
  *              P4.7 inside a loop. The LEDs are on when P1.0=1 and P4.7=1.
  *              The LED1 is initialized to be off and LED2 to be on.
@@ -29,10 +29,10 @@ void main(void)
     P1OUT &= ~BIT0;             // LED2 is OFF
     P4OUT |= BIT7;              // LED1 is ON
     unsigned int i = 0;
-    while(1){                   // Infinite loop
+    while(1){                        // Infinite loop
         for (i = 0; i < 50000; i++); // Delay 0.5s
-                                    // 0.5s on, 0.5s off => 1/(1s) = 1Hz
-        P1OUT ^= BIT0;             // Toggle LED1
-        P4OUT ^= BIT7;             // Toggle LED1
+                                     // 0.5s on, 0.5s off => 1/(1s) = 1Hz
+        P1OUT ^= BIT0;               // Toggle LED1
+        P4OUT ^= BIT7;               // Toggle LED1
     }
 }
