04343545ef60d450610c892f9f7561164717abf7
dthornton22 <68617419+dthornton22@users.noreply.github.com>
2020-10-10 02:11:37 -0500

    Update Lab_01_D1.c

--- Lab_01/Lab_01_D1.c
+++ Lab_01/Lab_01_D1.c
@@ -4,7 +4,7 @@
  * Description: This C program calculates the factorial for several integers
 				and print out the result for both int (16-bit) and
 				long int data types (32-bit), decimally and hexadecimally.
- * Input: 		None
+ * Input: 		MAXF
  * Output: 		Factorials of integer numbers from 1 to MAXF
  * Author(s): 	Aleksandar Milenkovic, milenkovic@computer.org
  * Date:		Aug 16, 2017
@@ -14,14 +14,13 @@
 
 #define MAXF 16
 
-int ifact(int);			// Prototype of ifact (integer)
-long int lifact(int);	// Prototype of lifact (long interger)
+int ifact(int);						// Prototype of ifact (integer)
+long int lifact(int);				// Prototype of lifact (long interger)
 
 int main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD;		// Stop watchdog timer to prevent time out
-	int i;							// Step counter
-	int a;							// Result for type int 
+	int i, a;						// Step counter and result for type int 
 	long int b;						// Result for type long int
 	// Print header
 	printf("( i)!	:  int-dec,  int-hex :	long int-dec,	long int-hex;\n");
