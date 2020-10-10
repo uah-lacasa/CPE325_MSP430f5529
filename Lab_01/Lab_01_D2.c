9164bcc62e5474a7e47c9d73c21022aeec4a1e45
dthornton22 <68617419+dthornton22@users.noreply.github.com>
2020-10-10 02:14:20 -0500

    Update Lab_01_D2.c

--- Lab_01/Lab_01_D2.c
+++ Lab_01/Lab_01_D2.c
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------------
  * File: 		Lab_01_D2.c
  * Function: 	This C code will calculate the power of a given base.
- * Description: This program calculates the por of a given
+ * Description: This program calculates the power of a given
  *				base using recursive function call.
  * Input:		None
  * Output:		Base raised to the given power.
@@ -12,35 +12,34 @@
 #include <math.h>
 #include "msp430.h"
 
-long int exponential(int, int); // Function prototype
+long int exponential(int, int); 			// Function prototype
 
 int main(void)
 {
-	WDTCTL = WDTPW + WDTHOLD;		// Stop watchdog timer to prevent time out
+	WDTCTL = WDTPW + WDTHOLD;				// Stop watchdog timer to prevent time out
 
-	int base = 12;		//the base
-	int por = 4;		//the exponent, must be positive and 1 or greater
-	long int ansr = exponential(base, por);		//find the por
+	int base = 12;							// Base
+	int power = 4;							// Exponent, must be greater than 1 
+	long int ansr = exponential(base, power);	//find the power
 
-	printf("%d raised to the %d por is %ld", base, por, ansr);
+	printf("%d raised to the %d power is %ld", base, power, ansr);
 
 	return 0;
 }
 
-//This function recursively calculates a por given a base and a por
-long int exponential(int base, int por)
+//This function recursively calculates a power given a base and a power
+long int exponential(int base, int power)
 {
-	long int ansr;		//the solution
-	if (por > 1)			//if the current call has por > 1 than call again
+	long int ansr;						// Solution
+	if (power > 1)						//if the current call has power > 1 than call again
 	{
-		por--;
-		ansr = base*exponential(base, por);
+		power--;
+		ansr = base*exponential(base, power);
 	}
 
-	else					//else the por = 1 and the ansr is the base
+	else					// else the power = 1 and the ansr is the base
 	{
 		ansr = base;
 	}
-
-	return ansr;			//return the ansr
+	return ansr;			// return the ansr
 }
